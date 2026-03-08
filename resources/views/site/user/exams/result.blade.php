a2eb8b7ff503c0b9d8506c6c449e9d9b8bfce988
Esgerov Enver <enver.esgerov0106@gmail.com>
2026-03-08 20:55:58 +0400

    first

--- resources/views/site/user/exams/result.blade.php
+++ resources/views/site/user/exams/result.blade.php
@@ -97,6 +97,7 @@
                             {{ __('site.correct_answer') }}:
 
                             @if($answer->question->type === 'multiple_choice')
+                                @dd($answer)
                                 @php
                                     $correctData = correctOptionData($answer->question->options ?? []);
                                 @endphp
